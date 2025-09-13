55240c3bd1b0f45b7fd3e0eb0f0d398478c7ec25
Boniich <ezequieldbo25@gmail.com>
2025-09-12 21:45:36 -0300

    Agrego atributos y metodos faltantes

--- src/modelo/Persona.java
+++ src/modelo/Persona.java
@@ -1,6 +1,42 @@
 package modelo;
 
-public class Persona {
+import java.time.LocalDate;
 
+public abstract class Persona {
+
+	private int idPersona;
 	private String nombre;
+	private String apellido;
+	private long dni;
+	private LocalDate fechaNacimiento;
+	
+	protected String getNombre() {
+		return nombre;
+	}
+	protected void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	protected String getApellido() {
+		return apellido;
+	}
+	protected void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	protected long getDni() {
+		return dni;
+	}
+	protected void setDni(long dni) {
+		this.dni = dni;
+	}
+	protected LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	protected void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	protected int getIdPersona() {
+		return idPersona;
+	}
+	
+	
 }
