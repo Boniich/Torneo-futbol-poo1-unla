853b49872c7023493ebaa2d28148e429cc6c0f1c
oalan4750 <oalan4750@gmail.com>
2025-09-14 17:43:03 -0300

    Creacion clase Equipo y Estadistica

---
+++ src/modelo/Equipo.java
@@ -0,0 +1,66 @@
+package modelo;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Equipo {
+
+	private final int idEquipo;
+	private String codigo;
+	private String nombre;
+	private List<Jugador> jugadores;
+	private Entrenador entrenador;
+	
+	public Equipo(int idEquipo, String codigo, String nombre, Entrenador entrenador) {
+		
+		this.idEquipo = idEquipo;
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.entrenador = entrenador;
+		this.jugadores = new ArrayList<>();
+	}
+	
+	public int getIdEquipo(){
+		return idEquipo;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo=codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+	public List<Jugador> getJugadores(){
+		return jugadores;
+	}
+	public void agregarJugador(Jugador jugador) { //falta la funcion mayor de edad de jugador, despues se agrega
+        //if (jugador.) {             
+            jugadores.add(jugador);
+        //} else {
+          //  throw new IllegalArgumentException("El jugador debe ser mayor de edad");
+        //}
+    }
+	public Entrenador getEntrenador() {
+		return entrenador;
+	}
+	public void setEntrenador(Entrenador entrenador) {
+		this.entrenador=entrenador;
+	}
+
+	@Override
+	public String toString() {
+		return "Equipo [idEquipo=" + idEquipo 
+				+ ", codigo=" + codigo 
+				+ ", nombre=" + nombre 
+				//falta las clases jugador y entrenador
+				//+ ", jugadores= "+ jugadores
+				//+ ", entrenador= " + entrenador
+				+ "]";
+	}
+	
+}
---
+++ src/modelo/Estadistica.java
@@ -0,0 +1,58 @@
+package modelo;
+
+public class Estadistica {
+	
+	private final int idEstadistica;
+	private int goles;
+	private int asistencias;
+	private int minutosJugados;
+	private Jugador jugador;
+	
+	public Estadistica(int idEstadistica, int goles, int asistencias, int minutosJugados, Jugador jugador) {
+		this.idEstadistica = idEstadistica;
+		this.goles = goles;
+		this.asistencias = asistencias;
+		this.minutosJugados = minutosJugados;
+		this.jugador = jugador;
+	}
+	
+	public int getIdEstadistica() {
+		return idEstadistica;
+	}
+	public int getGoles() {
+		return goles;
+	}
+	public void setGoles(int goles) {
+		this.goles=goles;
+	}
+	public int getAsistencias() {
+		return asistencias;
+	}
+	public void setAsistencias(int asistencias) {
+		this.asistencias = asistencias;
+	}
+	public int getMinutosJugados() {
+		return minutosJugados;
+	}
+	public void setMinutosJugados(int minutosJugados) {
+		this.minutosJugados = minutosJugados;
+	}
+	public Jugador getJugador() {
+		return jugador;
+	}
+	public void setJugador(Jugador jugador) {
+		this.jugador = jugador;
+	}
+
+	@Override
+	public String toString() {
+		return "Estadistica [idEstadistica=" + idEstadistica 
+				+ ", goles=" + goles 
+				+ ", asistencias=" + asistencias 
+				+ ", minutosJugados=" + minutosJugados 
+				//falta la clase Jugador
+				//+", jugador=" + jugador
+				+ "]";
+	}
+	
+}
