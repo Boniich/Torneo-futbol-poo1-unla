e348491a741e4451101a5aa0b974d15e5f0cf292
Boniich <ezequieldbo25@gmail.com>
2025-09-15 18:45:40 -0300

    eliminamos el idpersona y cambiamos visibilidad de los get y set

--- src/modelo/Persona.java
+++ src/modelo/Persona.java
@@ -4,38 +4,41 @@ import java.time.LocalDate;
 
 public abstract class Persona {
 
-	private int idPersona;
 	private String nombre;
 	private String apellido;
 	private long dni;
 	private LocalDate fechaNacimiento;
 	
-	protected String getNombre() {
-		return nombre;
-	}
-	protected void setNombre(String nombre) {
+	public Persona(String nombre, String apellido, long dni, LocalDate fechaNacimiento) {
 		this.nombre = nombre;
-	}
-	protected String getApellido() {
-		return apellido;
-	}
-	protected void setApellido(String apellido) {
 		this.apellido = apellido;
-	}
-	protected long getDni() {
-		return dni;
-	}
-	protected void setDni(long dni) {
 		this.dni = dni;
-	}
-	protected LocalDate getFechaNacimiento() {
-		return fechaNacimiento;
-	}
-	protected void setFechaNacimiento(LocalDate fechaNacimiento) {
 		this.fechaNacimiento = fechaNacimiento;
 	}
-	protected int getIdPersona() {
-		return idPersona;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public long getDni() {
+		return dni;
+	}
+	public void setDni(long dni) {
+		this.dni = dni;
+	}
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
 	}
 	
 	
