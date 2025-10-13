5a4d4432825cb355adbc8e8ad187aaf766336583
Boniich <ezequieldbo25@gmail.com>
2025-10-13 19:37:37 -0300

    agrega validaciones a la clase estadistica

--- src/modelo/Estadistica.java
+++ src/modelo/Estadistica.java
@@ -8,12 +8,12 @@ public class Estadistica {
 	private int minutosJugados;
 	private Jugador jugador;
 	
-	public Estadistica(int idEstadistica, int goles, int asistencias, int minutosJugados, Jugador jugador) {
+	public Estadistica(int idEstadistica, int goles, int asistencias, int minutosJugados, Jugador jugador) throws Exception {
 		this.idEstadistica = idEstadistica;
-		this.goles = goles;
-		this.asistencias = asistencias;
-		this.minutosJugados = minutosJugados;
-		this.jugador = jugador;
+		this.setGoles(goles);
+		this.setAsistencias(asistencias);
+		this.setMinutosJugados(minutosJugados);
+		this.setJugador(jugador);
 	}
 	
 	public int getIdEstadistica() {
@@ -22,25 +22,29 @@ public class Estadistica {
 	public int getGoles() {
 		return goles;
 	}
-	public void setGoles(int goles) {
+	public void setGoles(int goles) throws Exception {
+		if(goles < 0) throw new Exception("Error: Los goles no pueden ser inferiores a 0!");
 		this.goles=goles;
 	}
 	public int getAsistencias() {
 		return asistencias;
 	}
-	public void setAsistencias(int asistencias) {
+	public void setAsistencias(int asistencias) throws Exception {
+		if(asistencias < 0) throw new Exception("Error: Las asistencias no pueden ser inferiores a 0!");
 		this.asistencias = asistencias;
 	}
 	public int getMinutosJugados() {
 		return minutosJugados;
 	}
-	public void setMinutosJugados(int minutosJugados) {
+	public void setMinutosJugados(int minutosJugados) throws Exception {
+		if(minutosJugados < 0) throw new Exception("Error: Los minutos jugados no pueden ser inferiores a 0!");
 		this.minutosJugados = minutosJugados;
 	}
 	public Jugador getJugador() {
 		return jugador;
 	}
-	public void setJugador(Jugador jugador) {
+	public void setJugador(Jugador jugador) throws Exception {
+		if(jugador == null) throw new Exception("Error: El jugador no puede estar vacio!");
 		this.jugador = jugador;
 	}
 
@@ -51,7 +55,7 @@ public class Estadistica {
 				+ ", asistencias=" + asistencias 
 				+ ", minutosJugados=" + minutosJugados 
 				+", jugador=" + jugador
-				+ "]";
+				+ "]\n";
 	}
 	
 }
